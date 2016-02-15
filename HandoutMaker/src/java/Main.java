e285c68cd877bee14b779591d60031101e728d4b
demfehlinsta <julianbrinkmann@gmx.de>
2016-02-15 20:02:04 +0100

    Mehr Grundgerüständerungen

--- HandoutMaker/src/java/Main.java
+++ HandoutMaker/src/java/Main.java
@@ -1,7 +1,11 @@
 package java;
 
 import java.awt.Color;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main 
 {
@@ -24,13 +28,26 @@ public class Main
 		}
 		
 		Style style;
-		do
+		try 
 		{
-			style = loadNextStyle(styleFile, true);
-			applyStyle(targetFile, style);
+			BufferedReader reader = new BufferedReader(new FileReader(styleFile));
+			reader.mark(100);;
+			
+			do
+			{
+				style = loadNextStyle(reader, true);
+				applyStyle(targetFile, style);
+			}
+			while(!style.equals(loadNextStyle(reader, false)));
+		} 
+		catch (FileNotFoundException e)
+		{	
+			e.printStackTrace();
+		} 
+		catch (IOException e)
+		{
+			e.printStackTrace();
 		}
-		while(!style.equals(loadNextStyle(styleFile, false)));
-
 		
 		System.exit(0);
 	}
@@ -40,30 +57,117 @@ public class Main
 		if(style.identifier.equals("(!§$%&/()=?)"))
 		{
 			System.out.println("Error loading style identifier; Check your style sheet in line: " + lineNumber);
+			return;
 		}
 		
-		//style anwenden
+		if(style.identifier.equals("indent")) //oder was auch immer du als option dafür verwenden willst
+		{
+			//style anwenden (indent)
+		}
+		else
+		{
+			//style anwenden (non-indent)
+		}
 	}
 
-	private static Style loadNextStyle(File styleFile, boolean continueToNext) //same here, dachte mir man geht so reihe für reihe das style sheet durch und applied das nacheinander, das boolean sagt aus ob man zur nächsten Zeile gehen soll wegen der while-do-schleife (s.o.)
-	{
+	private static Style loadNextStyle(BufferedReader reader, boolean continueToNext) throws IOException //same here, dachte mir man geht so reihe für reihe das style sheet durch und applied das nacheinander, das boolean sagt aus ob man zur nächsten Zeile gehen soll wegen der while-do-schleife (s.o.)
+	{	
+		Style style = new Style();
+		String content = reader.readLine();
+		
+		//Identifier auslesen
+		content.trim();	
+		style.identifier = content.substring(0, findIdentifierEnd(content));
+		content = content.replaceFirst(style.identifier + ";", "");
+		style.identifier.replaceFirst(String.valueOf(")+ String.valueOf("), "indent").replaceAll(String.valueOf('"'), "");
+		
+		while(!content.equals(""))
+		{
+			//Anderes auslesen
+			String temp = content.substring(0, findEndOfEntry(content));
+			content = content.replaceFirst(temp, "");
+			
+			switch (temp.substring(0, findEquals(temp)))
+			{
+				case "style":
+				//TODO	
+					break;
+				case "size":
+				//TODO
+					break;
+				case "type":
+				//TODO
+					break;
+				case "underlinded":
+					//TODO
+					break;
+				case "cursive":
+				//TODO
+					break;
+				case "lineDistance":
+					//TODO
+						break;
+				case "color":
+					//TODO
+						break;
+				default: System.out.println("Error loading style property in line: " + lineNumber);				
+			}	
+		}
+		
 		if(continueToNext)
 		{
 			lineNumber++;
 		}
+		else
+		{
+			reader.reset();
+		}
 		
-		//TODO Nameing conventions
-		
-		//style in der line laden und returnen
-		
-		return null;
+		reader.mark(100);
+		return style;
+	}
+	
+	private static int findIdentifierEnd(String s)
+	{
+		for(int i = 1; i < s.length(); i++)
+		{
+			if(s.charAt(i) == Character.valueOf('"'))
+			{
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	private static int findEndOfEntry(String s)
+	{
+		for(int i = 1; i < s.length(); i++)
+		{
+			if(s.charAt(i) == Character.valueOf(';'))
+			{
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	private static int findEquals(String s)
+	{
+		for(int i = 1; i < s.length(); i++)
+		{
+			if(s.charAt(i) == Character.valueOf('='))
+			{
+				return i;
+			}
+		}
+		return -1;
 	}
 	
 	private static class Style
 	{
 		//füg Variablen hinzu, wenn dir welche einfallen, hab bestimmt net alle, hau die dann auch unten in den constructor
 		private short size, format; //format = Blocksatz, zentriert etc., mir fiel kein besserer Name ein, datentyp muss vllt gewechselt werden, fürs erste: 0=rechts, 1 = mitte, 2 = links, 3 = block
-		private String type, identifier;
+		private String style, identifier;
 		private boolean cursive, underlined;
 		private float lineDistance;
 		private Color color; //Kein Plan wie dat mit Farben abläuft, fürs erste mal die Java-Version benutzt 
@@ -72,7 +176,7 @@ public class Main
 		{
 			identifier = "(!§$%&/()=?)"; //ersma random zeichen, damit dat nich gesucht wird, wenn die id fehlt
 			size = 12;	//ab hier so standardeinstellungen
-			type = "Times New Roman";
+			style = "Times New Roman";
 			cursive = false;	
 			underlined = false;
 			format = 0;
@@ -80,11 +184,11 @@ public class Main
 			color = Color.BLACK;
 		}
 		
-		private Style(String identifier, short size, short format, String type, boolean cursive, boolean underlined, float lineDistance, Color color)
+		private Style(String identifier, short size, short format, String style, boolean cursive, boolean underlined, float lineDistance, Color color)
 		{
 			this.identifier = identifier;
 			this.size = size;
-			this.type = type;
+			this.style = style;
 			this.cursive = cursive;
 			this.underlined = underlined;
 			this.format = format;
