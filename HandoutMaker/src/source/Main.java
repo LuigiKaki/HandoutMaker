8e08658d6ebeb7a97c60f0a65d5c9ce24eb78cdc
demfehlinsta <julianbrinkmann@gmx.de>
2016-02-17 21:43:38 +0100

    small fix

--- HandoutMaker/src/source/Main.java
+++ HandoutMaker/src/source/Main.java
@@ -1,11 +1,6 @@
 package source;
 
-import java.awt.Color;
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.HashMap;
 
 import source.style.Style;
@@ -43,14 +38,8 @@ public class Main
 		System.exit(0);
 	}
 
-	private static void applyStyle(File target, Style style) //WIP und so (WIP = work in progress f�r die Ungebildeten)
+	private static void applyStyle(File target, Style style) //WIP und so 
 	{
-		if (style.identifier.equals("(!�$%&/()=?)"))
-		{
-			System.out.println("Error loading style identifier; Check your style sheet!");
-			return;
-		}
-
 		if (style.identifier.equals("indent")) //oder was auch immer du als option daf�r verwenden willst
 		{
 			//style anwenden (indent)
