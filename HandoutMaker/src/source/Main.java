943581914c9a8920910c61c7abce3b10213b21e4
demfehlinsta <julianbrinkmann@gmx.de>
2016-02-17 15:56:41 +0100

    Parserupdate

--- HandoutMaker/src/source/Main.java
+++ HandoutMaker/src/source/Main.java
@@ -9,10 +9,10 @@ import java.io.IOException;
 import java.util.HashMap;
 
 import source.style.Style;
+import source.style.StyleParser;
 
 public class Main 
 {
-	private static short lineNumber = 0;
 	public static HashMap<String, Style> styles = new HashMap<String, Style>();
 	
 	public static void main(String[] args)
@@ -32,31 +32,10 @@ public class Main
 			System.out.println("Target could not be found. Make sure the path and spelling are correct!");
 			System.exit(0); 
 		}
-		
-		Style style;
 		try 
 		{
-			BufferedReader reader = new BufferedReader(new FileReader(styleFile));
-			
-			switch (reader.readLine().trim())
-			{
-				case "normal":
-					reader.mark(100);
-					do
-					{
-						style = loadNextStyle(reader, true);
-						styles.put(style.identifier, style);
-					}
-					while(!style.equals(loadNextStyle(reader, false)));
-					reader.close();
-					break;
-				case "indent":
-					//TODO HAU MA DEINE SCHEISSE HIER REIN
-					break;
-				default:
-					System.out.println("Make sure you define normal or indent reading mode in the first line!");
-					break;
-			}
+			styleFile = StyleParser.formatFile(styleFile);
+			StyleParser.loadStyles(styleFile);
 		} 
 		catch (FileNotFoundException e)
 		{	
@@ -73,7 +52,7 @@ public class Main
 	{
 		if(style.identifier.equals("(!�$%&/()=?)"))
 		{
-			System.out.println("Error loading style identifier; Check your style sheet in line: " + lineNumber);
+			System.out.println("Error loading style identifier; Check your style sheet!");
 			return;
 		}
 		
@@ -85,87 +64,5 @@ public class Main
 		{
 			//style anwenden (non-indent)
 		}
-	}
-
-	private static Style loadNextStyle(BufferedReader reader, boolean continueToNext) throws IOException //same here, dachte mir man geht so reihe f�r reihe das style sheet durch und applied das nacheinander, das boolean sagt aus ob man zur n�chsten Zeile gehen soll wegen der while-do-schleife (s.o.)
-	{	
-		Style style = new Style();
-		String content = reader.readLine();
-		
-		//Identifier auslesen
-		content.trim();	
-		style.identifier = content.substring(0, findCharacter(content, '"'));
-		content = content.replaceFirst(style.identifier + ";", "");
-		style.identifier = style.identifier.replaceFirst(String.valueOf('"') + String.valueOf('"'), "indent").replaceAll(String.valueOf('"'), "");
-		
-		while(!content.equals(""))
-		{
-			//Anderes auslesen
-			String temp = content.substring(0, findCharacter(content, ';')).replaceAll(";", "");
-			content = content.replaceFirst(temp + ";", "");
-			
-			switch (temp.substring(0, findCharacter(temp, '=')))
-			{
-				case "style":
-					style.style = temp.substring(findCharacter(temp, '=') + 1, temp.length()).trim();
-					temp = "";
-					break;
-				case "size":
-					style.size = Short.parseShort(temp.substring(findCharacter(temp, '=') + 1, temp.length()).trim());
-					temp = "";
-					break;
-				case "format":
-				//sollte f�rs erste passen, format datentyp wird vllt noch ge�ndert
-					style.format = Short.parseShort(temp.substring(findCharacter(temp, '=') + 1, temp.length()).trim());
-					temp = "";
-					break;
-				case "underlinded":
-					style.underlined = Boolean.parseBoolean(temp.substring(findCharacter(temp, '=') + 1, temp.length()).trim());
-					temp = "";
-					break;
-				case "cursive":
-					style.cursive = Boolean.parseBoolean(temp.substring(findCharacter(temp, '=') + 1, temp.length()).trim());
-					temp = "";
-					break;
-				case "lineDistance":
-					style.lineDistance = Float.parseFloat(temp.substring(findCharacter(temp, '=') + 1, temp.length()).trim());
-					temp = "";
-					break;
-				case "color":
-				//TODO kp ob das so funktioniert oder ich n fettes switch case daf�r brauch
-					style.color = Color.getColor(temp.substring(findCharacter(temp, '=') + 1, temp.length()).trim(), Color.BLACK);
-					temp = "";
-					break;
-				case "bold":
-					style.bold = Boolean.parseBoolean(temp.substring(findCharacter(temp, '=') + 1, temp.length()).trim());
-					break;
-				default: 
-					System.out.println("Error loading style property in line: " + lineNumber);
-					break;
-			}	
-		}
-		
-		if(continueToNext)
-		{
-			lineNumber++;
-		}
-		else
-		{
-			reader.reset();
-		}		
-		reader.mark(100);
-		return style;
-	}
-	
-	private static int findCharacter(String s, Character c)
-	{
-		for(int i = 1; i < s.length(); i++)
-		{
-			if(s.charAt(i) == Character.valueOf(c))
-			{
-				return i;
-			}
-		}
-		return -1;
 	}
 }
--- HandoutMaker/src/source/style/StyleParser.java
+++ HandoutMaker/src/source/style/StyleParser.java
@@ -1,24 +1,78 @@
 package source.style;
 
+import java.awt.Color;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
+
+import source.Main;
 
 public class StyleParser
 {
-	//hab dir ma deine HashMap abgerippt, is jz in Main, schreib da rein
-	File src;
-	public StyleParser(File f)throws Exception
+	public static File formatFile(File f) 
 	{
-		BufferedReader reader = new BufferedReader(new FileReader(f));
-		String parsingchar = "  ";
-		for(String s = reader.readLine(); s != null; reader.readLine()){
-			if(s.indexOf(parsingchar) == -1){
-				
-				
-				reader.close();
+		//TODO das gegebene file formattieren, sodass mein parser es verarbeiten kann; wenn es schon passt einfach so lassen; änderungen am input vornehmen und dann wieder returnen;
+		return f;
+	}
+	
+	public static void loadStyles(File f) throws IOException, FileNotFoundException
+	{	
+		BufferedReader reader = new BufferedReader(new FileReader(f));	
+
+		for(String data = reader.readLine().trim(); !data.equals(""); data = reader.readLine().trim())
+		{
+			String[] content = data.split(";");
+			
+			if(!(content[0].startsWith(String.valueOf('"')) && content[0].endsWith(String.valueOf('"'))) || content[0].replaceAll(String.valueOf('"'), "").trim().equals(""))
+			{
+				System.out.println("Error loading style identifier!");
+				continue;
 			}
+			
+			Style style = new Style();
+			style.identifier = content[0].replaceAll(String.valueOf('"'), "").trim();
+			
+			for(int i = 1; i < content.length; i++)
+			{			
+				content[i] = content[i].trim();
+				
+				switch (content[i].substring(0, content[i].indexOf("=")))
+				{
+					case "style":
+						style.style = content[i].substring(content[i].indexOf("=") + 1, content[i].length()).trim();
+						break;
+					case "size":
+						style.size = Short.parseShort(content[i].substring(content[i].indexOf("=") + 1, content[i].length()).trim());							
+						break;
+					case "format": //erst mal okay
+						style.format = Short.parseShort(content[i].substring(content[i].indexOf("=") + 1, content[i].length()).trim());
+						break;
+					case "underlinded":
+						style.underlined = Boolean.parseBoolean(content[i].substring(content[i].indexOf("=") + 1, content[i].length()).trim());
+						break;
+					case "cursive":
+						style.cursive = Boolean.parseBoolean(content[i].substring(content[i].indexOf("=") + 1, content[i].length()).trim());
+						break;
+					case "lineDistance":
+						style.lineDistance = Float.parseFloat(content[i].substring(content[i].indexOf("=") + 1, content[i].length()).trim());	
+						break;
+					case "color": //soll wohl erst mal gehen
+						style.color = Color.getColor(content[i].substring(content[i].indexOf("=") + 1, content[i].length()), Color.BLACK);						
+						break;
+					case "bold":
+						style.bold = Boolean.parseBoolean(content[i].substring(content[i].indexOf("=") + 1, content[i].length()).trim());
+						break;
+					default: 
+						System.out.println("Error loading style property in line: " + i);
+						break;
+				}	
+			}
+			Main.styles.put(style.identifier, style);
 		}
+		reader.close();
 	}
 }
+
