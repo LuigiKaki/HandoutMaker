416e9071505c63fe84c952e303b7d36c3e4780a2
LuigiKaki <kakitatus@gmail.com>
2016-03-05 20:45:56 +0100

    - UID hinzugefügt, damit der eclipse Fehler verschwindet

--- HandoutMaker/src/source/exceptions/StyleNotFoundException.java
+++ HandoutMaker/src/source/exceptions/StyleNotFoundException.java
@@ -2,6 +2,7 @@ package source.exceptions;
 
 public class StyleNotFoundException extends Exception
 {
+	private static final long serialVersionUID = 5001L;
 	String name;
 	public StyleNotFoundException(String styleName)
 	{
